57ac9bca0f0f6b0c0b40ad6435df9c83c2f25308
danny hong <Danny.Hong@hoplun.com>
2024-10-15 17:57:49 +0800

    创建Demo.SourceGenerator项目

---
+++ Demo.SourceGenerator/DataPermissionInterceptorSourceGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Demo.SourceGenerator
+{
+    public class DataPermissionInterceptorSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
